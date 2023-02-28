15ad817a2ec68454c3c4762a1d372a86a8c1200d
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-02-27 22:58:48 -0500

    Gui

---
+++ src/GameGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class GameGUI {
+    private JButton button1;
+    private JButton heloButton;
+    private JButton button3;
+    private JTextArea welcomeToTheGameTextArea;
+
+}
